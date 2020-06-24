f70457922b73ee4674427a34a5b176daa608d075
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-06-24 10:38:58 +0100

    stop reading + exceptions

--- rtpi.py
+++ rtpi.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
 from sys import argv
+import requests
+
+def mkdct():
+   try:
+      with open("stops.txt", "r") as fd:
+         full = fd.readlines()
+         dct = {}
+         for line in full:
+            k, v = line.strip().split(",")
+            dct[k], dct[v] = v, v
+   except FileNotFoundError:
+      print(" --- Error. No stops.txt file found --- ")
+      return {}
+   return dct
 
 def main():
-   a = argv[1:]
-   print(a)
+   arg = argv[1:]
+   dct = mkdct()
+   if len(arg) == 0:
+      stop = "6030"
+   elif arg[0] in dct:
+      stop = dct[arg[0]]
+   else:
+      stop = arg[0]
 
+   print(stop)
 
 if __name__ == "__main__":
    main()
